103214cb75f7028f9ae4470440c35e7a56b35956
Richard Scott Smith <scott.smith@isostech.com>
2013-12-20 11:34:47 -0700

    Added a JSON web service message to return JSON data.

--- src/main/java/net/raescott/dojo/HomeController.java
+++ src/main/java/net/raescott/dojo/HomeController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+import java.util.TreeMap;
 
 @Controller
 @RequestMapping("/")
@@ -12,5 +16,12 @@ public class HomeController {
 	public String homePage(ModelMap model) {
 		model.addAttribute("message", "Page content");
 		return "home";
+	}
+
+	@RequestMapping(value = "example-data", method = RequestMethod.GET)
+	public @ResponseBody Map<String, String> exampleJson() {
+		Map<String, String> response = new TreeMap<String, String>();
+		response.put("status", "true");
+		return response;
 	}
 }
