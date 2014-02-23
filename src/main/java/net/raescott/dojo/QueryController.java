917c8a657fe6af5bca65c2b8e23e7bbe8970bfbf
Richard Scott Smith <scott.smith@isostech.com>
2014-02-22 22:38:38 -0700

    Removed AJAX query handlers from the Events and Query Controller since they are not using them.

--- src/main/java/net/raescott/dojo/QueryController.java
+++ src/main/java/net/raescott/dojo/QueryController.java
@@ -4,10 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Map;
-import java.util.TreeMap;
 
 @Controller
 @RequestMapping("query")
@@ -16,13 +12,6 @@ public class QueryController {
 	public String queryPage(ModelMap model) {
 		model.addAttribute("message", "Page content");
 		return "home";
-	}
-
-	@RequestMapping(value = "example-data", method = RequestMethod.GET)
-	public @ResponseBody Map<String, String> exampleJson() {
-		Map<String, String> response = new TreeMap<String, String>();
-		response.put("status", "true");
-		return response;
 	}
 
 }
