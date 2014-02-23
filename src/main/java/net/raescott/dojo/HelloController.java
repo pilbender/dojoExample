c829852dbb1f1aff5eff806a01f5b27db1743f38
Richard Scott Smith <scott.smith@isostech.com>
2014-02-22 21:57:30 -0700

    Added the Hello Dojo example from the Dojo Toolkit example website.

---
+++ src/main/java/net/raescott/dojo/HelloController.java
@@ -0,0 +1,16 @@
+package net.raescott.dojo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("hello")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String calculatorPage(ModelMap model) {
+		model.addAttribute("message", "Page content");
+		return "hello";
+	}
+}
