3dbfc86e798a6064c8912d3931929a897da257ac
Richard Scott Smith <scott.smith@isostech.com>
2015-02-04 16:13:49 -0700

    Added some of the code for using a Dojo Store in a widget.  Things are working except for the declarative widget part, which renders but does not update the values in the widget.

--- src/main/webapp/resources/js/PersonWidget/PersonWidget.js
+++ src/main/webapp/resources/js/PersonWidget/PersonWidget.js
@@ -6,10 +6,14 @@
 define([
 	"dojo/_base/declare",
 	"dojo/request",
+	"dojo/query",
+	"dojo/store/Memory",
+	"dojo/store/Observable",
+	"dojo/when",
 	"dijit/_WidgetBase",
 	"dijit/_TemplatedMixin",
 	"dojo/text!./templates/PersonWidget.html"
-], function(declare, request, _WidgetBase, _TemplatedMixin, template){
+], function(declare, request, query, Memory, Observable, when, _WidgetBase, _TemplatedMixin, template){
 	// TODO: I do not yet understand why the return value is used in this case and not in others.
 	return declare("PersonWidget", [_WidgetBase, _TemplatedMixin], {
 		// Some default values for our author
@@ -17,6 +21,32 @@ define([
 		name: "No Name",
 		age: "No Age",
 		state: "No State",
+
+		/*
+		store: null,
+		_setStoreAttr: function (store) {
+			// notify of a property change
+			this._set('store', store);
+			if (this.store) {
+				this.render();
+			}
+		},
+
+		render: function () {
+			var self = this;
+			// query for all items and render the results
+			when(this.store.query()).then(function (results) {
+				results.forEach(function (item) {
+					self.renderItem(item);
+				});
+			});
+		},
+
+		renderItem: function (item) {
+			// render an item
+			console.debug(JSON.stringify(item));
+		},
+		*/
 
 		// Accessors and Mutators
 		// TODO: These getters and setters can be deleted.  They are not being used right now.
@@ -31,15 +61,15 @@ define([
 		templateString: template,
 
 		// A class to be applied to the root node in our template
-		baseClass: "personWidget",
+		baseClass: "personWidget"
 
-		// TODO: This is starting to seem like a bad idea.  It can be completely deleted.
+		/*// TODO: This is starting to seem like a bad idea.  It can be completely deleted.
 		// Make the AJAX call to the service
 		postCreate: function () {
 			// Essential to keeping a reference to the _Widget predefined dom node.  Once we're in this closure, we
 			// can't get to it.
 			var domNode = this.domNode;
-			var name = this.domNode.nameNode;
+			var name = domNode;
 
 			request("widget/person-data", {
 				handleAs: "json"
@@ -49,19 +79,16 @@ define([
 				//personContainer.innerHTML = "Illustrating that you can grab data and put it in the attach point manually, like this: " + dummyData.name;
 
 				// Create our widget and place it
-				//personContainer.innerHTML = "";  // TODO: Seems like there ought to be a better way to clear the dom.
-				//var widget = new PersonWidget(dummyData).placeAt(personContainer);
 				console.debug("name: " + name);
 				console.debug("this.name: " + this.name);
 				console.debug("domNode: " + domNode.toString());
-				this.name.innerHTML = dummyData.name;
+				name.innerHTML = dummyData.name;
 				console.debug("name: " + name);
 				console.debug("this.name: " + this.name);
-				age = dummyData.age;
-				state = dummyData.state;
-				//widget.startup(); // TODO: Not needed here, but it is considered a best practice to have it.
+				age.innerHTML = dummyData.age;
+				state.innerHTML = dummyData.state;
 			});
-		}
+		}*/
 	});
 });
 
