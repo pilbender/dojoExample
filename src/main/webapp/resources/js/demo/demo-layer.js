40b8792c8699fbdb735b6dbe7b8de91d4bdc913a
Richard Scott Smith <scott.smith@isostech.com>
2015-03-05 11:05:40 -0700

    Added all necessary Dojo toolkit dependencies to the new demo-layer.

--- src/main/webapp/resources/js/demo/demo-layer.js
+++ src/main/webapp/resources/js/demo/demo-layer.js
@@ -1,4 +1,17 @@
 require([
+	"dojo/dom",
+	"dojo/request",
+	"dojo/json",
+	"dojo/_base/array",
+	"dojo/domReady",
+	"dojo/_base/declare",
+	"dojo/parser",
+	"dojo/ready",
+	"dijit/_WidgetBase",
+	"dojo/dom-construct",
+	"dijit/_TemplatedMixin",
+	"dojo/request",
+	"dojo/promise/all",
 	'demo/demo-layer',
 	'PersonWidget/PersonWidget'
 ]);
